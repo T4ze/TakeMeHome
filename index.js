e8b91590fb341d2620aaf8c18cbb83ee352ed34d
Florent Youinou <youino_f@epita.fr>
2017-03-09 01:24:47 +0100

    project: clean

--- index.js
+++ index.js
@@ -16,34 +16,5 @@ mb.on('ready', function ready () {
 });
 
 mb.on('after-create-window', function ready () {
-  //mb.window.openDevTools();
 })
 
-/*
-
-const electron = require('electron');
-
-// Module to control application life.
-const app = electron.app;
-
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
-
-function createWindow() {
-mainWindow = new BrowserWindow({
-        'minWidth': 350,
-        'minHeight': 400,
-        'width': 350,
-        'height': 400
-    });
-
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-}
-
-app.on('ready', createWindow);
-*/
